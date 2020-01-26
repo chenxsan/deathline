602029c1312bf5a858bb85102e5a374e51941de5
chenxsan <chenxsan@gmail.com>
2020-01-26 16:32:05 +0800

    Add border to header

--- components/RenderTime.tsx
+++ components/RenderTime.tsx
@@ -7,7 +7,11 @@ interface Props {
 function RenderTime({ time, items }: Props) {
   return (
     <div className="mb-10 mt-2">
-      <h3 className="lg:ml-8">{time}</h3>
+      <h3 className="lg:ml-8">
+        <span className="border-2 border-l-0 border-t-0 border-r-0 border-black">
+          {time}
+        </span>
+      </h3>
       <div className="lg:ml-12">
         {items.map((item: Item) => (
           <RenderItem item={item} key={item.id} />
