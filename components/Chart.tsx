84381cddfe2ce6da208f550cf177eeae3b999f09
chenxsan <chenxsan@gmail.com>
2020-01-29 20:05:56 +0800

    Clean code

--- components/Chart.tsx
+++ components/Chart.tsx
@@ -1,25 +1,15 @@
 import { Suspense } from 'react';
 import data from '../data';
-import StackedBar from './StackedBar'
+import StackedBar from './StackedBar';
 import useClient from '../hooks/useClient';
-const dates = Object.keys(data).sort((a, b) => {
+const dates: string[] = Object.keys(data).sort((a, b) => {
   return new Date(a).getTime() - new Date(b).getTime();
 });
-const males = dates
-  .map(date => {
-    return Object.values(data[date])
-      .flat()
-      .filter(item => item.gender === 'male');
-  })
+
+const statistic: number[] = dates
+  .map(date => Object.values(data[date]).flat())
   .map(arr => arr.length);
-const females = dates
-  .map(date => {
-    return Object.values(data[date])
-      .flat()
-      .filter(item => item.gender === 'female');
-  })
-  .map(arr => arr.length);
-const statistic = dates.map(date => Object.values(data[date]).flat()).map(arr => arr.length)
+
 function Chart() {
   const client = useClient();
   const data = {
